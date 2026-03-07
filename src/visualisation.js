a9c32a65411de869bee42867b6402fbb7978fd7b
sophieshankland <sophiexshankland@gmail.com>
2026-03-07 22:33:45 +0000

    improved aesthetics and moved around axes titles

--- src/visualisation.js
+++ src/visualisation.js
@@ -1,7 +1,7 @@
 const width = 900;
 const height = 300;
 
-const margin = {top: 30, right: 20, bottom: 30, left: 60};
+let margin = {top: 40, right: 30, bottom: 40, left: 70};
 
 const svg = d3.select("#chart");
 
@@ -52,7 +52,7 @@ d3.csv("../data/productivity.csv", d => {
     // Makes colour gradient based on average productivity score for the hour
     const color = d3.scaleSequential()
         .domain([1,5])
-        .interpolator(d3.interpolateRgbBasis(["#b10900", "#ffce3a", "#3b912a" ]));
+        .interpolator(d3.interpolateRgbBasis(["#d32a21", "#ffc61b", "rgb(39, 172, 13)" ]));
 
     // Drawing, positioning, and colouring heatmap cells 
     g.selectAll("rect")
@@ -67,42 +67,43 @@ d3.csv("../data/productivity.csv", d => {
         .attr("fill", d => d.avg === 0 ? "#ffffff" : color(d.avg))
         .attr("stroke", "#333")
         .attr("stroke-width", 0.5)
-        .on("mouseover",(event,d)=>{
+        .on("mouseover", (event, d) => {
 
-            tooltip
-            .style("opacity",1)
-            .html(`
-            Day: ${d.day}<br>
-            Hour: ${d.hour}<br>
-            Avg: ${d.avg.toFixed(2)}
-            `);
-            })
-
-        .on("mousemove",(event)=>{
-
-            tooltip
-            .style("left",(event.pageX+10)+"px")
-            .style("top",(event.pageY+10)+"px");
-
+    // Add the average number inside the cell
+    g.append("text")
+        .attr("class", "hover-label")
+        .attr("x", x(d.hour) + x.bandwidth() / 2)
+        .attr("y", y(d.day) + y.bandwidth() / 2 + 4)
+        .attr("text-anchor", "middle")
+        .text(d.avg.toFixed(2));
         })
 
-        .on("mouseout",()=>{
+        .on("mousemove", (event, d) => {
+            // Position tooltip above the cell
+            const [mouseX, mouseY] = d3.pointer(event, svg.node());
 
-            tooltip.style("opacity",0);
-
+            tooltip
+                .style("left", (mouseX + margin.left + 10) + "px")
+                .style("top", (mouseY + margin.top - 25) + "px");
         })
 
-        .on("click",(event,d)=>{
+        .on("mouseout", () => {
+            // Hide tooltip
+            tooltip.style("opacity", 0);
 
-            showDistribution(d);
+            // Remove the in‑cell label
+            d3.selectAll(".hover-label").remove();
+        })
 
+        .on("click", (event, d) => {
+          showDistribution(d);
         });
 
     const xAxis = d3.axisBottom(x)
-        .tickValues([0,4,8,12,16,20,23]);
+      .tickFormat(d => `${String(d).padStart(2, "0")}:00`);
 
     const yAxis = d3.axisLeft(y)
-        .tickFormat(d => ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"][d]);
+        .tickFormat(d => ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"][d]);
 
     g.append("g")
         .attr("transform", `translate(0,${chartHeight})`)
@@ -113,70 +114,67 @@ d3.csv("../data/productivity.csv", d => {
 
     createLegend(color);
 
-function showDistribution(d){
+function showDistribution(d) {
 
   const container = d3.select("#distribution");
-
   container.html("");
 
   const width = 400;
-  const height = 200;
+  const height = 220;
+
+  const margin = { top: 20, right: 20, bottom: 40, left: 40 };
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 
   const svg = container
-  .append("svg")
-  .attr("width",width)
-  .attr("height",height);
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-  // Makes x-axis
+  const g = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
   const x = d3.scaleBand()
-  .domain([1,2,3,4,5])
-  .range([40,width-20])
-  .padding(0.2);
+    .domain([1, 2, 3, 4, 5])
+    .range([0, innerWidth])
+    .padding(0.2);
 
-  // Makes y-axis
   const y = d3.scaleLinear()
-  .domain([0,d3.max(d.freq)])
-  .range([height-30,10]);
+    .domain([0, d3.max(d.freq)])
+    .range([innerHeight, 0]);
 
   const barColor = d3.scaleSequential()
     .domain([1, 5])
-    .interpolator(d3.interpolateRgbBasis(["#d73027", "#fee08b", "#1a9850" ]));
-  // Adds each bar
-  svg.selectAll("rect")
-  .data(d.freq)
-  .enter()
-  .append("rect")
-  .attr("x",(v,i)=>x(i+1))
-  .attr("y",v=>y(v))
-  .attr("width",x.bandwidth())
-  .attr("height",v=>height-30-y(v))
-  .attr("fill", (v, i) => barColor(i + 1))
-  .attr("stroke", "#333")
-  .attr("stroke-width", 0.5)
+    .interpolator(d3.interpolateRgbBasis(["#d73027", "#ffde21", "#1a9850"]));
 
-  svg.append("g")
-  .attr("transform",`translate(0,${height-30})`)
-  .call(d3.axisBottom(x));
+  g.selectAll("rect")
+    .data(d.freq)
+    .enter()
+    .append("rect")
+    .attr("x", (v, i) => x(i + 1))
+    .attr("y", v => y(v))
+    .attr("width", x.bandwidth())
+    .attr("height", v => innerHeight - y(v))
+    .attr("fill", (v, i) => barColor(i + 1))
+    .attr("stroke", "#333")
+    .attr("stroke-width", 0.5);
 
-  const maxY = d3.max(d.freq);
+  g.append("g")
+    .attr("transform", `translate(0, ${innerHeight})`)
+    .call(d3.axisBottom(x));
 
-  svg.append("g")
-    .attr("transform","translate(40,0)")
-    .call(
-      d3.axisLeft(y)
-        .tickValues(d3.range(0, maxY + 1)) 
-    );
+  g.append("g")
+    .call(d3.axisLeft(y).ticks(d3.max(d.freq)));
 
 
   svg.append("text")
-  .attr("x",width/2)
-  .attr("y",height)
-  .attr("text-anchor","middle")
-  .text(`Distribution (Day ${d.day}, Hour ${d.hour})`)
-  .style("font-family", "sans-serif")
-  .style("font-size", "12px");
-
-
+    .attr("x", width / 2)
+    .attr("y", margin.top / 1.5 - 5)
+    .attr("text-anchor", "middle")
+    .style("font-family", "georgia")
+    .style("font-size", "12px")
+    .text(`Productivity Levels (Day ${d.day}, Hour ${d.hour})`);
 }
 });
 
